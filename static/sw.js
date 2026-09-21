2fdd177c47065801f05fa3e34a1475e3c8b45389
daswitchchannel-rgb <daswitchchannel@gmail.com>
2026-09-21 14:39:07 +0530

    Add service worker for web app

---
+++ static/sw.js
@@ -0,0 +1,14 @@
+const CACHE="siddhi-vinayak-v10";
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["/","/static/index.html"])));
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return response;
+    }).catch(()=>cached))
+  );
+});
